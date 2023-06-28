aebd44dcfe15e69be4b8416952352a42710750b8
kelysot <kelysot@gmail.com>
2023-06-28 15:45:54 +0300

    Add upload photos functionality to aws S3 when the admin creates a new company.

--- services/company.py
+++ services/company.py
@@ -1,10 +1,17 @@
+import os
+import uuid
 from http.client import HTTPException
 
 from asyncpg import UniqueViolationError
 
+from APIs.s3 import S3Service
+from constants import TEMP_FILE_FOLDER
 from db import database
 from models import RoleType, company
 from services.helpers import HelperService
+from utils.helpers import decode_photo
+
+s3 = S3Service()
 
 
 class CompanyService:
@@ -15,9 +22,14 @@ class CompanyService:
 
     @staticmethod
     async def create_company(company_data):
+        # Upload photo to aws S3.
         encoded_photo = company_data.pop("encoded_photo")
         extension = company_data.pop("extension")
-        company_data["logo_url"] = "temp"
+        name = f"{uuid.uuid4()}.{extension}"
+        path = os.path.join(TEMP_FILE_FOLDER, name)
+        decode_photo(path, encoded_photo)
+        company_data["logo_url"] = s3.upload(path, name, extension)
+        os.remove(path)
 
         # Try to insert the company data into the DB.
         try:
